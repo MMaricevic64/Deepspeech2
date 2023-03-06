f653978fac24a57b216551cfbbd019c1fd31e815
Mario Maricevic <mmaricevic123@gmail.com>
2023-03-06 18:18:12 +0100

    Updated .py scripts

--- dataset.py
+++ dataset.py
@@ -6,15 +6,15 @@ os.chdir(path)
 
 filenames = list()
 transcriptions = list()
-       
+
 for name in sorted(os.listdir("txt")):
     filenames.append(os.path.splitext(name)[0])
     with open(os.path.join(path, "txt", name), "r") as file:
-        transcriptions.append("".join(file.readline()..strip().split()))
+        transcriptions.append(" ".join(file.readline().strip().split()))
 
 with open("dataset.csv", "w", newline='', encoding='utf-8') as file:
     writer = csv.writer(file, delimiter='|')
     writer.writerow(["filename", "transcription"])
     writer.writerows(zip(filenames, transcriptions))
-    
+
 print("Dataset (.csv) is ready!")
